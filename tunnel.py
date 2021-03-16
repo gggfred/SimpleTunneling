3a6a3a8f866c6a2265ce3ee1a32f9ed6ebf93ad2
Galo Guzman <galo.guzman@outlook.com>
2021-03-16 13:37:44 -0500

    tunnel tcp and udp classes created

---
+++ tunnel.py
@@ -0,0 +1,150 @@
+from PySide2.QtCore import QCoreApplication, Slot, QTimer
+from PySide2.QtNetwork import QTcpServer, QTcpSocket, QUdpSocket, QHostAddress, QAbstractSocket
+
+class TunnelTCP():
+    name = ""
+    localServerPort = 0
+    localTCPSocket = None
+
+    clientPeerAddress = 0
+
+    remoteIPAddress = 0
+    remoteTCPport = 0
+
+    def __init__(self, name = "N/A"):
+        self.name = name
+        self.localServer = QTcpServer()
+        self.localServer.newConnection.connect(self.newConnectionProc)
+
+        self.remoteTCPSocket = QTcpSocket()
+        self.remoteTCPSocket.stateChanged.connect(self.onRemoteStateChanged)
+        self.remoteTCPSocket.readyRead.connect(self.onRemote2Local)
+
+        self.timerOpenRemote = QTimer()
+        self.timerOpenRemote.timeout.connect(self.tryOpenRemote)
+        self.timerOpenRemote.setInterval(1000)
+
+    def setRemoteIPAddress(self, ip):
+        self.remoteIPAddress = ip
+
+    def setRemoteTCPPort(self, port):
+        self.remoteTCPport = port
+
+    def setServerPort(self, port):
+        self.localServerPort = port
+
+    def performListening(self):
+        self.localServer.listen(QHostAddress('0.0.0.0'), self.localServerPort)
+
+    @Slot()
+    def tryOpenRemote(self):
+        if self.remoteTCPSocket.isOpen() == False:
+            print('%s: Try to open %s:%s' % (self.name, self.remoteIPAddress, self.remoteTCPport))
+            self.remoteTCPSocket.connectToHost(QHostAddress(self.remoteIPAddress), self.remoteTCPport)
+
+    @Slot(QAbstractSocket.SocketState)
+    def onRemoteStateChanged(self, state):
+        if state == QAbstractSocket.HostLookupState:
+            print("%s: Lookup for Remote Host" % self.name)
+        elif state == QAbstractSocket.ConnectingState:
+            print("%s: Connecting to Remote Host" % self.name)
+        elif state == QAbstractSocket.ConnectedState:
+            print("%s: Connected to Remote Host" % self.name)
+        elif state == QAbstractSocket.UnconnectedState:
+            print("%s: Disconnected to Remote Host" % self.name)
+            self.remoteTCPSocket.close()
+            self.timerOpenRemote.stop()
+
+    @Slot(QAbstractSocket.SocketState)
+    def onLocalStateChanged(self, state):
+        if state == QAbstractSocket.UnconnectedState:
+            print("%s: Client disconnected" % self.name)
+            self.remoteTCPSocket.close()
+            self.timerOpenRemote.stop()
+
+    @Slot()
+    def newConnectionProc(self):
+        self.localTCPSocket = self.localServer.nextPendingConnection()
+        self.localTCPSocket.stateChanged.connect(self.onLocalStateChanged)
+        self.localTCPSocket.readyRead.connect(self.onLocal2Remote)
+
+        self.clientPeerAddress = self.localTCPSocket.peerAddress()
+
+        self.timerOpenRemote.start()
+
+        print("%s: %s client connected" % (self.name, self.clientPeerAddress.toString()))
+
+    @Slot()
+    def onLocal2Remote(self):
+        if self.remoteTCPSocket.isOpen():
+            data = self.localTCPSocket.readAll()
+            print("%s: L2R: %s" % (self.name, data))
+            self.remoteTCPSocket.write(data)
+
+    @Slot()
+    def onRemote2Local(self):
+        if self.localTCPSocket.isOpen():
+            data = self.remoteTCPSocket.readAll()
+            print("%s: R2L: %s" % (self.name, data))
+            self.localTCPSocket.write(data)
+
+class TunnelUDP():
+    name = ""
+    localListenPort = 0
+
+    remoteIPAddress = 0
+    remoteUDPport = 0
+
+    udpResponseIP = None
+    udpResponsePort = None
+
+    def __init__(self, name = "N/A"):
+        self.name = name
+        self.udpLocal = QUdpSocket()
+        self.udpLocal.readyRead.connect(self.udpL2R)
+
+        self.udpRemote = QUdpSocket()
+        self.udpRemote.readyRead.connect(self.udpR2L)
+
+    def setRemoteIPAddress(self, ip):
+        self.remoteIPAddress = ip
+
+    def setRemoteUDPPort(self, port):
+        self.remoteUDPport = port
+
+    def setBindPort(self, port):
+        self.localListenPort = port
+
+    def performBinding(self):
+        self.udpLocal.bind(QHostAddress('0.0.0.0'), self.localListenPort)
+
+    @Slot()
+    def udpL2R(self):
+        while self.udpLocal.hasPendingDatagrams() == True:
+            datagram = self.udpLocal.receiveDatagram()
+            data = datagram.data()
+            self.udpResponseIP = datagram.senderAddress()
+            self.udpResponsePort = datagram.senderPort()
+            print("%s: FROM: %s %s TO: %s %s" % (
+                self.name,
+                datagram.senderAddress().toString(),
+                datagram.senderPort(),
+                datagram.destinationAddress().toString(),
+                datagram.destinationPort()))
+            print("%s: L2R: %s" % (self.name, data))
+            self.udpRemote.writeDatagram(data, QHostAddress(self.remoteIPAddress), self.remoteUDPport)
+
+    @Slot()
+    def udpR2L(self):
+        while self.udpRemote.hasPendingDatagrams() == True:
+            datagram = self.udpRemote.receiveDatagram()
+            data = datagram.data()
+            print("%s: FROM: %s %s TO: %s %s" % (
+                self.name,
+                datagram.senderAddress().toString(),
+                datagram.senderPort(),
+                datagram.destinationAddress().toString(),
+                datagram.destinationPort()))
+            print("%s: R2L: %s" % (self.name, data))
+            self.udpLocal.writeDatagram(data, self.udpResponseIP, self.udpResponsePort)
+
--- tunneling.py
+++ tunneling.py
@@ -4,109 +4,36 @@ import sys
 from PySide2.QtCore import QCoreApplication, Slot, QTimer
 from PySide2.QtNetwork import QTcpServer, QTcpSocket, QUdpSocket, QHostAddress, QAbstractSocket
 
-REMOTE_CONTROLLER_IP = "192.168.0.1"
-REMOTE_TCP_CONTROLLER = 3001
-REMOTE_UDP_CONTROLLER = 52001
-REMOTE_TCP_TERMINAL = 19001
+from tunnel import TunnelTCP, TunnelUDP
 
-LOCAL_TCP_CONTROLLER = 3001
-LOCAL_UDP_CONTROLLER = 52001
-LOCAL_TCP_TERMINAL = 19001
+REMOTE_CONTROLLER_IP = "192.168.0.1"
+REMOTE_TCP_CONTROLLER_PORT = 3001
+REMOTE_UDP_CONTROLLER_PORT = 52001
+REMOTE_TCP_TERMINAL_PORT = 19001
+
+LOCAL_TCP_CONTROLLER_PORT = 3001
+LOCAL_UDP_CONTROLLER_PORT = 52001
+LOCAL_TCP_TERMINAL_PORT = 19001
 
 class App():
-    TCPLocal = None
-    udpResponseIP = None
-    udpResponsePort = None
-
     def __init__(self, *args, **kwargs):
-        self.tcpServer = QTcpServer()
-        self.tcpServer.newConnection.connect(self.procNewConn)
-        self.tcpServer.listen(QHostAddress('0.0.0.0'), LOCAL_TCP_CONTROLLER)
+        self.tunnel1 = TunnelTCP("Tunnel 1")
+        self.tunnel1.setServerPort(LOCAL_TCP_CONTROLLER_PORT)
+        self.tunnel1.setRemoteIPAddress(REMOTE_CONTROLLER_IP)
+        self.tunnel1.setRemoteTCPPort(REMOTE_TCP_CONTROLLER_PORT)
+        self.tunnel1.performListening()
 
-        self.tcpRemote = QTcpSocket()
-        self.tcpRemote.stateChanged.connect(self.onRemoteStateChanged)
-        self.tcpRemote.readyRead.connect(self.tcpR2L)
+        self.tunnel2 = TunnelTCP("Tunnel 2")
+        self.tunnel2.setServerPort(LOCAL_TCP_TERMINAL_PORT)
+        self.tunnel2.setRemoteIPAddress(REMOTE_CONTROLLER_IP)
+        self.tunnel2.setRemoteTCPPort(REMOTE_TCP_TERMINAL_PORT)
+        self.tunnel2.performListening()
 
-        self.udpLocal = QUdpSocket()
-        self.udpLocal.readyRead.connect(self.udpL2R)
-        self.udpLocal.bind(QHostAddress('0.0.0.0'), LOCAL_UDP_CONTROLLER)
-
-        self.udpRemote = QUdpSocket()
-        self.udpRemote.readyRead.connect(self.udpR2L)
-
-        self.timerOpenRemote = QTimer()
-        self.timerOpenRemote.timeout.connect(self.tryOpenRemote)
-        self.timerOpenRemote.setInterval(2000)
-        self.timerOpenRemote.start()
-
-    @Slot()
-    def tryOpenRemote(self):
-        if self.tcpRemote.isOpen() == False:
-            print('Try open')
-            self.tcpRemote.connectToHost(
-                QHostAddress(REMOTE_CONTROLLER_IP), REMOTE_TCP_CONTROLLER)
-
-    @Slot()
-    def procNewConn(self):
-        self.TCPLocal = self.tcpServer.nextPendingConnection()
-        self.TCPLocal.readyRead.connect(self.tcpL2R)
-        print("TCP IP: %s Server Connected" %
-              self.TCPLocal.peerAddress().toString())
-
-    @Slot(QAbstractSocket.SocketState)
-    def onRemoteStateChanged(self, state):
-        if state == QAbstractSocket.HostLookupState:
-            print("Lookup for Remote Host")
-        elif state == QAbstractSocket.ConnectingState:
-            print("Connecting to Remote Host")
-        elif state == QAbstractSocket.ConnectedState:
-            print("Connected to Remote Host")
-        elif state == QAbstractSocket.UnconnectedState:
-            print("Disconnected to Remote Host")
-            self.tcpRemote.close()
-
-    @Slot()
-    def tcpL2R(self):
-        data = self.TCPLocal.readAll()
-        print("TCP L2R: %s" % data)
-        self.tcpRemote.write(data)
-
-    @Slot()
-    def tcpR2L(self):
-        data = self.tcpRemote.readAll()
-        print("TCP R2L: %s" % data)
-        self.TCPLocal.write(data)
-
-    @Slot()
-    def udpL2R(self):
-        while self.udpLocal.hasPendingDatagrams() == True:
-            datagram = self.udpLocal.receiveDatagram()
-            data = datagram.data()
-            self.udpResponseIP = datagram.senderAddress()
-            self.udpResponsePort = datagram.senderPort()
-            print("%s %s %s %s" % (
-                datagram.senderAddress().toString(),
-                datagram.senderPort(),
-                datagram.destinationAddress().toString(),
-                datagram.destinationPort()))
-            print("UDP L2R: %s" % data)
-            self.udpRemote.writeDatagram(
-                data, QHostAddress(REMOTE_CONTROLLER_IP), REMOTE_UDP_CONTROLLER)
-
-    @Slot()
-    def udpR2L(self):
-        while self.udpRemote.hasPendingDatagrams() == True:
-            datagram = self.udpRemote.receiveDatagram()
-            data = datagram.data()
-            print("%s %s %s %s" % (
-                datagram.senderAddress().toString(),
-                datagram.senderPort(),
-                datagram.destinationAddress().toString(),
-                datagram.destinationPort()))
-            print("UDP R2L: %s" % data)
-            self.udpLocal.writeDatagram(
-                data, self.udpResponseIP, self.udpResponsePort)
-
+        self.tunnel3 = TunnelUDP("Tunnel 3")
+        self.tunnel3.setBindPort(LOCAL_UDP_CONTROLLER_PORT)
+        self.tunnel3.setRemoteIPAddress(REMOTE_CONTROLLER_IP)
+        self.tunnel3.setRemoteUDPPort(REMOTE_UDP_CONTROLLER_PORT)
+        self.tunnel3.performBinding()
 
 if __name__ == "__main__":
     app = QCoreApplication(sys.argv)
